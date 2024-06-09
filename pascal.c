5c91053903737af179d889fe40f30f027933df4b
Shivakumar V S <157830150+Shivakumar-001@users.noreply.github.com>
2024-06-09 10:08:38 +0530

    Update pascal.c

--- pascal.c
+++ pascal.c
@@ -3,31 +3,31 @@
 int factorial(int n);
 void main()
 {
-		int i,n,j;
-		clrscr();
-		printf("Enter number of rows:n");
-		scanf("%d",&n);
-		for(i=0;i<n;i++)
-		{
-				for(j=0;j<=(n-i-2);j++)    
-				{
-					printf(" ");
-				}
-				for(j=0;j<=i;j++)
-				{
-					printf("%d",factorial(i)/(factorial(j)*factorial(i-j)));
-				}
-				printf("/n");
+	int i,n,j;
+	clrscr();
+	printf("Enter number of rows:n");
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+			for(j=0;j<=(n-i-2);j++)    
+			{
+				printf(" ");
+			}
+			for(j=0;j<=i;j++)
+			{
+				printf("%d",factorial(i)/(factorial(j)*factorial(i-j)));
+			}
+			printf("/n");
 	}
 	getch();
 }
 int factoral(int n)
 {
-		int c;
-		int result = 1;
-		for(c=1;c<=n;c++)
-{
+	int c;
+	int result = 1;
+	for(c=1;c<=n;c++)
+	{
 		result=result*c;
-}
-		return result;
+	}
+	return result;
 }
