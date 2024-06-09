ae8d9f5b6a121f5fbf34e51aaaef06bde953311f
Shivakumar V S <157830150+Shivakumar-001@users.noreply.github.com>
2024-06-09 10:06:17 +0530

    Create pascal.c
    Program to display Pascal traingle using binomial function

---
+++ pascal.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<conio.h>
+int factorial(int n);
+void main()
+{
+		int i,n,j;
+		clrscr();
+		printf("Enter number of rows:n");
+		scanf("%d",&n);
+		for(i=0;i<n;i++)
+		{
+				for(j=0;j<=(n-i-2);j++)    
+				{
+					printf(" ");
+				}
+				for(j=0;j<=i;j++)
+				{
+					printf("%d",factorial(i)/(factorial(j)*factorial(i-j)));
+				}
+				printf("/n");
+	}
+	getch();
+}
+int factoral(int n)
+{
+		int c;
+		int result = 1;
+		for(c=1;c<=n;c++)
+{
+		result=result*c;
+}
+		return result;
+}
